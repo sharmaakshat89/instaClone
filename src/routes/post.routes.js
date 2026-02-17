1594841d72cac1e2f89137dd94bc9c756cebf10a
sharmaakshat89 <sharmaakshat89@gmail.com>
2026-02-17 19:08:30 +0530

    added middlewares to post routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -8,11 +8,11 @@ const upload=require('../middlewares/upload')
 
 
 
-postRouter.get('/',postController.getPostController)
+postRouter.get('/',identifyUser,postController.getPostController)
 
-postRouter.get('/details/:postid', postController.getPostDetailsController)  // returns details about a specific post that has the id suffixed in the url, also check whether the post belongs to the user who used this api endpoint
+postRouter.get('/details/:postid', identifyUser,postController.getPostDetailsController)  // returns details about a specific post that has the id suffixed in the url, also check whether the post belongs to the user who used this api endpoint
 
-postRouter.post('/',upload.single('image' ) ,postController.createPostController)
+postRouter.post('/',upload.single('image' ), identifyUser ,postController.createPostController)
 // upload.single('image')----> whatever is the name of key in which the pic is uploaded in the form @frontend is used here
 
 
